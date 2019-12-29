17494d45beec098134aac43bfef3cc8daf9f6c52
Austin <Austin@DESKTOP-LQ4S9F6>
2019-12-29 14:51:38 -0600

    Vector3 completed

--- src/crt/main/Launcher.java
+++ src/crt/main/Launcher.java
@@ -1,9 +1,18 @@
 package crt.main;
 
+import crt.math.Vector3;
+
 public class Launcher {
 
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		Vector3 vec1 = new Vector3(1,5,7);
+		Vector3 vec2 = new Vector3(1,5,7);
+		Vector3 vecCrossed = vec1.cross(vec2);
+		float vecDotted = vec1.dot(vec2);
+		vecCrossed.print();
+		System.out.println(vecDotted);
+		Vector3 vecProjected = vec1.scalarProjection(vec2);
+		vecProjected.print();
 	}
 	
 }
